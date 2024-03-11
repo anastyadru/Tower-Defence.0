f6a7b6931f03fe46257586668e79a8c25092bb31
Nastia <adruts@mail.ru>
2024-03-11 21:54:58 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Добавила скрипт AudioController

--- Assets/Client/Scripts/AudioToggler.cs
+++ Assets/Client/Scripts/AudioToggler.cs
@@ -19,4 +19,21 @@ public class AudioToggler : MonoBehaviour
             AudioListener.volume = 0f;
         }
     }
+    
+    public void OnOffSounds()
+    {
+        if (!isOn)
+        {
+            AudioListener.volume = 1f;
+            isOn = true;
+        }
+        else
+        {
+            AudioListener.volume = 0f;
+            isOn = false;
+        }
+
+        PlayerPrefs.SetInt("SoundOn", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+    }
 }
