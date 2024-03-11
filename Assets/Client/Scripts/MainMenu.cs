58812d05916a983bc49d639989dd1db80b348d7c
Nastia <adruts@mail.ru>
2024-03-11 17:46:52 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Создала кнопку Exit

--- Assets/Client/Scripts/MainMenu.cs
+++ Assets/Client/Scripts/MainMenu.cs
@@ -10,4 +10,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    
+    public void ExitGame()
+    {
+        Debug.Log("Игра закрылась");
+        Application.Quit();
+    }
 }
