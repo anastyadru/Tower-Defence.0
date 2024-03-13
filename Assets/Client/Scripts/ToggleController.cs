12f62348d5c07370a0ba2e8ad519726a8298ccb9
Nastia <adruts@mail.ru>
2024-03-13 16:49:39 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Создала скрипт ToggleController

---
+++ Assets/Client/Scripts/ToggleController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ToggleController : MonoBehaviour
+{
+
+    public void ToggleMusic(bool enabled)
+    {
+        
+    }
+}
