6b56b436962d3320c5d264f6e9de23610cb13cdb
Nastia <adruts@mail.ru>
2024-03-18 16:58:53 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт GameM

--- Assets/Client/Scripts/GameM.cs
+++ Assets/Client/Scripts/GameM.cs
@@ -7,4 +7,21 @@ public class GameM : MonoBehaviour
 {
     public static GameM instance;
     [SerializeField] private Text _healthText;
+    [SerializeField] private int _health = 100;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            Debug.Log("Defeat");
+        }
+
+        _healthText.text = _health.ToString();
+    }
 }
