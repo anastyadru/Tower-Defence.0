0108121e575431779bce10359aa20e15bb9a6838
Nastia <adruts@mail.ru>
2024-03-20 13:29:56 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Изменила скрипт класса Enemy

--- Assets/Client/Scripts/Enemy.cs
+++ Assets/Client/Scripts/Enemy.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _damage;
+    [SerializeField] private int _health = 10;
+    [SerializeField] private TextMesh _healthText;
 
     public NavMeshAgent agent;
     public GameObject EndCube;
@@ -14,11 +16,24 @@ public class Enemy : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         EndCube = GameObject.FindGameObjectWithTag("EndCube");
+        
+        _healthText.text = _health.ToString();
     }
     
     private void Update()
     {
         agent.SetDestination(EndCube.transform.position);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        _healthText.text = _health.ToString();
     }
 
     private void OnTriggerEnter(Collider other)
