705964e3694362331ce5497b69e1fd07762b0a54
Nastia <adruts@mail.ru>
2024-03-21 15:22:17 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Создала скрипт Player

---
+++ Assets/Client/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform _head;
+    [Space(5)]
+    [SerializeField] private float _range = 15;
+    [SerializeField] private float _rotationSpeed = 3;
+    
+    
+}
