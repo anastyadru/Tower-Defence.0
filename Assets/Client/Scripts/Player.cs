74a337fcd00d79d614d9d1005d8f2295598c8450
Nastia <adruts@mail.ru>
2024-03-21 18:24:59 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт Player

--- Assets/Client/Scripts/Player.cs
+++ Assets/Client/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
             if (distance < shortestDistance)
             {
                 shortestDistance = distance;
-                
+                _nearestEnemy = enemy;
             }
         }
 
