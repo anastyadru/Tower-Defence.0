f18f449772647454d0876476b4f37ba04fee8466
Nastia <adruts@mail.ru>
2024-03-25 17:18:32 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Изменила скрипт Tile

--- Assets/Client/Scripts/Tile.cs
+++ Assets/Client/Scripts/Tile.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    [SerializeField] private GameObject _playerPreview;
+    [SerializeField] private GameObject _player;
 
+    private GameObject _crPlayerPreview;
+    private bool _build;
+    private bool _spawned;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _build = !_build;
+
+            if (_crPlayerPreview != null)
+            {
+                Destroy(_crPlayerPreview);
+            }
+        }
+    }
 }
