2df728b5f3c440184b673a8a76d38ec79d37b4be
Nastia <adruts@mail.ru>
2024-03-25 15:17:45 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Изменила префаб пули

--- Assets/Client/Scripts/Bullet.cs
+++ Assets/Client/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private int _damage = 1;
     [SerializeField] private float _speed = 50;
+    
     private Transform _target;
 
     public void Find(Transform target)
