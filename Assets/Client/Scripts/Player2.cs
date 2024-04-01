c196bf1e24916ec917d8ebe5d8c60883353f7688
Nastia <adruts@mail.ru>
2024-04-01 18:48:56 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт Player2

--- Assets/Client/Scripts/Player2.cs
+++ Assets/Client/Scripts/Player2.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Player2 : MonoBehaviour
 {
+    [SerializeField] private Transform _head;
+    [SerializeField] private Transform[] _firePoints;
+    [SerializeField] private GameObject _bullet;
 
+    [SerializeField] private float _range = 15;
+    [SerializeField] private float _rotationSpeed = 3;
+    [SerializeField] private float _fireRate = 1;
+
+    private Transform _target;
+    private Enemy _nearestEnemy;
+    private float _countdown;
 }
