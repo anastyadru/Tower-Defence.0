e0d4274ce843906fd98456bd04f773f0cbeb5d66
Nastia <adruts@mail.ru>
2024-04-02 20:46:28 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт PlayerManager

--- Assets/Client/Scripts/PlayerManager.cs
+++ Assets/Client/Scripts/PlayerManager.cs
@@ -6,6 +6,15 @@ public class PlayerManager : MonoBehaviour
 {
     private PlayerBtn playerBtnPressed;
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
+        }
+    }
+
     public void SelectedPlayer(PlayerBtn playerSelected)
     {
         playerBtnPressed = playerSelected;
