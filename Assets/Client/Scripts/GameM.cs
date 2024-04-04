313e9ec02780da4d38df5a4037fdc195b3e239f4
Nastia <adruts@mail.ru>
2024-04-04 15:52:14 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт Enemy

--- Assets/Client/Scripts/GameM.cs
+++ Assets/Client/Scripts/GameM.cs
@@ -88,6 +88,11 @@ public class GameM : MonoBehaviour
         }
 
         _healthText.text = "Health: " + _health;
+        
+        if (_waveIndex > 0 && damage >= 10)
+        {
+	        damage -= 10;
+        }
     }
 
 	IEnumerator Spawn()
@@ -99,7 +104,7 @@ public class GameM : MonoBehaviour
 				Instantiate(_enemy, _startCube.transform.position, _enemy.transform.rotation);
 				yield return new WaitForSeconds(_spawnInterval);
 			}
-			TakeDamage(10 * _enemyCounts[_waveIndex]);
+			// TakeDamage(10 * _enemyCounts[_waveIndex]);
 			_waveIndex++;
 		}
 	}
