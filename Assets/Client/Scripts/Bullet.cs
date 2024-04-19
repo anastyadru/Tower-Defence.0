d548d04fb2f8793d5c1f2963ff0dd9342566cd12
Nastia <adruts@mail.ru>
2024-04-19 19:26:04 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Откорректировала код класса Bullet

--- Assets/Client/Scripts/Bullet.cs
+++ Assets/Client/Scripts/Bullet.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private int _damage = 100;
     [SerializeField] private float _speed = 50;
     
     private Transform _target;
-    private int _bulletsRequired; // Количество пуль, необходимых для убийства врага
-    
-    private int _currentWave = 1; // Текущая волна
 
-    public void Find(Transform target, int bulletsRequired)
+    public void Find(Transform target)
     {
         _target = target;
-        _bulletsRequired = bulletsRequired + _currentWave - 1; // Устанавливаем количество пуль, учитывая текущую волну
     }
 
     private void Update()
@@ -26,27 +23,15 @@ public class Bullet : MonoBehaviour
 
             if(direction.magnitude <= distance)
             {
-                _bulletsRequired--; // Уменьшаем количество пуль, использованных для убийства врага
-                if (_bulletsRequired <= 0)
-                {
-                    _target.GetComponent<Enemy>().TakeDamage(int.MaxValue);
-                    Destroy(gameObject);
-                }
+                _target.GetComponent<Enemy>().TakeDamage(_damage);
+                Destroy(gameObject);
             }
-            else
-            {
-                transform.Translate(direction.normalized * distance, Space.World);
-            }
+
+            transform.Translate(direction.normalized * distance, Space.World);
         }
         else
         {
             Destroy(gameObject);
         }
-    }
-    
-    public void SetWave(int wave)
-    {
-        _currentWave = wave;
-        _bulletsRequired = wave; // Устанавливаем количество пуль, учитывая текущую волну
     }
 }
