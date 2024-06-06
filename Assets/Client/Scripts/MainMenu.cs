7105243e8336fe8b7dac019a249e33befd80af63
Nastia <adruts@mail.ru>
2024-06-06 21:41:18 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Добавила во все скрипты // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.

--- Assets/Client/Scripts/MainMenu.cs
+++ Assets/Client/Scripts/MainMenu.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- Assets/Client/Scripts/Tile.cs
+++ Assets/Client/Scripts/Tile.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
