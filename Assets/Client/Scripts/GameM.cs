b6a6a7c8a7535efb36b3e0c8f836fb311cfc213b
Nastia <adruts@mail.ru>
2024-06-12 18:01:45 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Изменила код GameM

--- Assets/Client/Scripts/GameM.cs
+++ Assets/Client/Scripts/GameM.cs
@@ -35,8 +35,8 @@ public class GameM : MonoBehaviour
   private bool _isWaveInProgress = false;
   private bool _waitingForPlay = true;
   
-  public event System.Action OnPlayButtonClicked;
-  public event System.Action OnStopWaves;
+  // public event System.Action OnPlayButtonClicked;
+  // public event System.Action OnStopWaves;
   
   public Button playButton;
 
