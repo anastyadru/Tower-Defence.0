4f7d3a8788acfab10876907a123185b8d110d9d9
Nastia <adruts@mail.ru>
2024-06-16 12:01:11 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт GameInstaller

--- Assets/Client/Scripts/GameInstaller.cs
+++ Assets/Client/Scripts/GameInstaller.cs
@@ -1,17 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameInstaller : MonoInstaller
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public override void InstallBindings()
     {
         
     }
