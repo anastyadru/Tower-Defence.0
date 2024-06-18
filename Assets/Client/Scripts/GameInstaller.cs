c97cad2fb8966de1bc28cb3355617e7938a3ce2e
Nastia <adruts@mail.ru>
2024-06-18 09:43:09 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт GameInstaller

--- Assets/Client/Scripts/GameInstaller.cs
+++ Assets/Client/Scripts/GameInstaller.cs
@@ -5,9 +5,8 @@ public class GameInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
-        Container.Bind<PlayerManager>().AsSingle();
-        Container.Bind<GameM>().AsSingle();
-        Container.Bind<Player>().AsSingle();
-        Container.Bind<Player2>().AsSingle();
+        Container.Bind<Player>().FromComponentInHierarchy().AsSingle();
+        Container.Bind<PlayerManager>().FromComponentInHierarchy().AsSingle();
+        Container.Bind<Player2>().FromComponentInHierarchy().AsSingle();
     }
 }
