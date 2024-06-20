a9fc9ec7c3b5755369486f75176dca119ac34ed6
Nastia <adruts@mail.ru>
2024-06-20 13:16:58 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Создала скрипт IPoolable

---
+++ Assets/Client/Scripts/IPoolable.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPoolable
+{
+    void OnRelease();
+}
