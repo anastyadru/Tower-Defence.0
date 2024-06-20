766427d7d5354a269ca86a9806a62287beee8df7
Nastia <adruts@mail.ru>
2024-06-20 12:52:05 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт ObjectPool

--- Assets/Client/Scripts/ObjectPool.cs
+++ Assets/Client/Scripts/ObjectPool.cs
@@ -1,18 +1,11 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
