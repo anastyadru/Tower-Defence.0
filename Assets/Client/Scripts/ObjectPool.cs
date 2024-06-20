b8cda8b826bb05b684f534faf9e51d0370d6775c
Nastia <adruts@mail.ru>
2024-06-20 13:06:14 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт ObjectPool

--- Assets/Client/Scripts/ObjectPool.cs
+++ Assets/Client/Scripts/ObjectPool.cs
@@ -7,5 +7,73 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    public Bullet bulletPrefab;
     
+    public Dictionary<Type, Queue<IPoolable>> poolDictionary = new Dictionary<Type, Queue<IPoolable>>();
+
+    public void Start()
+    {
+        PrePool<BulletControllerPlayer>(bulletPrefabPlayer, 200, playerPoolDictionary);
+        PrePool<BulletControllerEnemy>(bulletPrefabEnemy, 200, enemyPoolDictionary);
+        PrePool<Enemy>(PrefabEnemy, 50, enemyPoolDictionary);
+    }
+
+    public void PrePool<T>(T prefab, int count, Dictionary<Type, Queue<IPoolable>> poolDict) where T : MonoBehaviour, IPoolable
+    {
+        Type type = typeof(T);
+        if (!poolDict.ContainsKey(type))
+        {
+            Queue<IPoolable> objectPool = new Queue<IPoolable>();
+            for (int i = 0; i < count; i++)
+            {
+                T obj = GameObject.Instantiate(prefab) as T;
+                obj.gameObject.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDict.Add(type, objectPool);
+        }
+    }
+
+    public T Get<T>(Dictionary<Type, Queue<IPoolable>> poolDict) where T : MonoBehaviour, IPoolable
+    {
+        Type type = typeof(T);
+        if (poolDict.ContainsKey(type) && poolDict[type].Count > 0)
+        {
+            IPoolable obj = poolDict[type].Dequeue();
+            return (T)obj;
+        }
+    
+        return null;
+    }
+
+    public void Release<T>(T poolableObject, Dictionary<Type, Queue<IPoolable>> poolDict) where T : MonoBehaviour, IPoolable
+    {
+        Type type = typeof(T);
+        if (poolDict.ContainsKey(type))
+        {
+            Queue<IPoolable> objectPool = poolDict[type];
+            objectPool.Enqueue(poolableObject);
+            poolableObject.OnRelease();
+        }
+    }
+    public BulletControllerPlayer GetPlayerBullet()
+    {
+        return Get<BulletControllerPlayer>(playerPoolDictionary);
+    }
+
+    public void ReleasePlayerBullet(BulletControllerPlayer bullet)
+    {
+        Release(bullet, playerPoolDictionary);
+    }
+
+    public BulletControllerEnemy GetEnemyBullet()
+    {
+        return Get<BulletControllerEnemy>(enemyPoolDictionary);
+    }
+
+    public void ReleaseEnemyBullet(BulletControllerEnemy bullet)
+    {
+        Release(bullet, enemyPoolDictionary);
+    }
 }
