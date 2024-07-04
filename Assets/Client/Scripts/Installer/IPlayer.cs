096c35b4e3d8cca1e415c2ef9ebef04f01693a5a
Nastia <adruts@mail.ru>
2024-07-04 21:30:40 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала IPlayer

--- Assets/Client/Scripts/Installer/IPlayer.cs
+++ Assets/Client/Scripts/Installer/IPlayer.cs
@@ -1,18 +1,7 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
-public class IPlayer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
