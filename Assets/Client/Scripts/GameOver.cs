6f8494911dba533bb82cfb75bf0b96d67fbb9e58
Nastia <adruts@mail.ru>
2024-07-10 19:08:19 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт GameOver

--- Assets/Client/Scripts/GameOver.cs
+++ Assets/Client/Scripts/GameOver.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
 
 public class GameOver : MonoBehaviour
 {
