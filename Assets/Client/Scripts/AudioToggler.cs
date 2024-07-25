aaa58b99df7ea8117dd858ada0c54da2d2cd7ee4
Nastia <adruts@mail.ru>
2024-07-25 16:39:49 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт AudioToggler

--- Assets/Client/Scripts/AudioToggler.cs
+++ Assets/Client/Scripts/AudioToggler.cs
@@ -1,41 +1,27 @@
-// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioToggler : MonoBehaviour
 {
-    public bool isOn;
-    
+    private const float soundOnVolume = 1f;
+    private const float soundOffVolume = 0f;
+
     private void Start()
     {
-        isOn = PlayerPrefs.GetInt("SoundOn", 1) == 1;
-        if (isOn)
-        {
-            AudioListener.volume = 1f;
-        }
-        else
-        {
-            AudioListener.volume = 0f;
-        }
+        SetAudioVolume(PlayerPrefs.GetInt("SoundOn", 1) == 1);
     }
-    
+
     public void OnOffSounds()
     {
-        if (!isOn)
-        {
-            AudioListener.volume = 1f;
-            isOn = true;
-        }
-        else
-        {
-            AudioListener.volume = 0f;
-            isOn = false;
-        }
-
+        bool isOn = AudioListener.volume == soundOffVolume;
+        SetAudioVolume(isOn);
         PlayerPrefs.SetInt("SoundOn", isOn ? 1 : 0);
         PlayerPrefs.Save();
+    }
+
+    private void SetAudioVolume(bool isOn)
+    {
+        AudioListener.volume = isOn ? soundOnVolume : soundOffVolume;
     }
 }
