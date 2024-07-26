d431c9c50a3947541ccaccda03dc11f0deaf261d
Nastia <adruts@mail.ru>
2024-07-26 19:46:28 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт Tile

--- Assets/Client/Scripts/Tile.cs
+++ Assets/Client/Scripts/Tile.cs
@@ -1,7 +1,5 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
