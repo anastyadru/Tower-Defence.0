edce75c70f2cb9b766ba7bb1769b52e244a02849
Nastia <adruts@mail.ru>
2024-07-26 18:10:40 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт ToggleController

--- Assets/Client/Scripts/ToggleController.cs
+++ Assets/Client/Scripts/ToggleController.cs
@@ -1,10 +1,7 @@
-// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class ToggleController : MonoBehaviour
 {
