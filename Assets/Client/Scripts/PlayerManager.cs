7cbe39f790c07cfbc0a1d1268d1e76a475147a9a
Nastia <adruts@mail.ru>
2024-07-26 18:08:55 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт PlayerManager

--- Assets/Client/Scripts/PlayerManager.cs
+++ Assets/Client/Scripts/PlayerManager.cs
@@ -1,9 +1,7 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Zenject;
 
 public class PlayerManager : MonoBehaviour
