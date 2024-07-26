333a7a55130583d3d73727ff3174271aefccd564
Nastia <adruts@mail.ru>
2024-07-26 18:04:54 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт PlayerBtn

--- Assets/Client/Scripts/PlayerBtn.cs
+++ Assets/Client/Scripts/PlayerBtn.cs
@@ -1,7 +1,5 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBtn : MonoBehaviour
@@ -10,10 +8,6 @@ public class PlayerBtn : MonoBehaviour
 
     public GameObject PlayerObject
     {
-        get
-        {
-            return playerObject;
-        }
-        
+        playerObject;
     }
 }
