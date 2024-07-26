962a924d64e8a8dd8378ad86c73c78f01a4dac1e
Nastia <adruts@mail.ru>
2024-07-26 17:44:44 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт MainMenu

--- Assets/Client/Scripts/MainMenu.cs
+++ Assets/Client/Scripts/MainMenu.cs
@@ -1,8 +1,5 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
