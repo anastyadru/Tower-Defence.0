086c54460c2cb1bc909dd95b2d7576ff707a62c3
Nastia <adruts@mail.ru>
2024-07-26 18:09:52 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт SliderController

--- Assets/Client/Scripts/SliderController.cs
+++ Assets/Client/Scripts/SliderController.cs
@@ -1,30 +1,31 @@
-// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
 {
     public Slider slider;
-    public float oldVolume;
 
     private void Start()
     {
-        oldVolume = slider.value;
-        if (!PlayerPrefs.HasKey("volume")) slider.value = 1;
-        else slider.value = PlayerPrefs.GetFloat("volume");
+        if (!PlayerPrefs.HasKey("volume")) 
+        {
+            PlayerPrefs.SetFloat("volume", 1f);
+            slider.value = 1f;
+        }
+        else 
+        {
+            slider.value = PlayerPrefs.GetFloat("volume");
+        }
     }
 
     private void Update()
     {
-        if (oldVolume != slider.value)
+        if (slider.value != PlayerPrefs.GetFloat("volume"))
         {
             PlayerPrefs.SetFloat("volume", slider.value);
             PlayerPrefs.Save();
-            oldVolume = slider.value;
         }
     }
 }
