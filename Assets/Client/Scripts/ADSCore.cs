8581729a3a21b74669606679c4e21afa5abb4cfd
Nastia <adruts@mail.ru>
2024-08-06 18:25:12 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт ADSCore

--- Assets/Client/Scripts/ADSCore.cs
+++ Assets/Client/Scripts/ADSCore.cs
@@ -1,18 +1,10 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ADSCore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
