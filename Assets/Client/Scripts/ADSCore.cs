51252df3dbdfd83c9ec7c7ce2cdf36d40ff54fce
Nastia <adruts@mail.ru>
2024-08-06 23:08:42 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт ADSCore

--- Assets/Client/Scripts/ADSCore.cs
+++ Assets/Client/Scripts/ADSCore.cs
@@ -1,7 +1,5 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ADSCore : MonoBehaviour
