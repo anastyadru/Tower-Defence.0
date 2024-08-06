715a1581c2354615ff601a2710435fcc926b8bd9
Nastia <adruts@mail.ru>
2024-08-07 00:01:35 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт ADSCore

--- Assets/Client/Scripts/ADSCore.cs
+++ Assets/Client/Scripts/ADSCore.cs
@@ -1,13 +1,13 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class ADSCore : MonoBehaviour
 {
     [SerializeField] private bool _testMode = true;
 
     private string _gameId = "5671873";
-
     private string _rewardedVideo = "Rewarded Android";
 
     void Start()
@@ -15,11 +15,11 @@ public class ADSCore : MonoBehaviour
         Advertisement.Initialize(_gameId, _testMode);
     }
 
-    public static void ShowAdsVideo(string placementId)
+    public void ShowAdsVideo()
     {
-        if (Advertisement.IsReady())
+        if (Advertisement.IsReady(_rewardedVideo))
         {
-            Advertisement.Show(placementId);
+            Advertisement.Show(_rewardedVideo);
         }
         else
         {
