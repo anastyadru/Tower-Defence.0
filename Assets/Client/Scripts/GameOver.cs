2306da2b326805935165907ed17085cc6589b46f
Nastia <adruts@mail.ru>
2024-08-08 13:54:27 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт GameOver

--- Assets/Client/Scripts/GameOver.cs
+++ Assets/Client/Scripts/GameOver.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
 
 public class GameOver : MonoBehaviour
 {
--- Assets/Client/Scripts/RewardedADS.cs
+++ Assets/Client/Scripts/RewardedADS.cs
@@ -1,10 +1,37 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
 
-public class RewardedAds : MonoBehaviour
+[RequireComponent(typeof(Button))]
+public class RewardedAds : MonoBehaviour, IUnityAdsListener
 {
+    [SerializeField] private bool _testMode = true;
+    [SerializeField] private Button _adsButton;
+    
+    private string _gameId = "5671873";
+    
+    private string _rewardedVideo = "Rewarded_Android";
+
+    void Start()
+    {
+        _adsButton = GetComponent<Button>();
+        _adsButton.interactable = Advertisement.IsReady(_rewardedVideo);
+
+        if (_adsButton)
+        {
+            _adsButton.onClick.AddListener(ShowRewardedVideo);
+        }
+
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(_gameId, true);
+    }
+
+    public void ShowRewardedVideo()
+    {
+        Advertisement.Show(_rewardedVideo);
+    }
+    
     
 }
