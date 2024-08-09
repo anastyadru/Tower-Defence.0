b2e57bd1a9359798d3a568af8132265f0e70e426
Nastia <adruts@mail.ru>
2024-08-09 12:53:07 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт AdsCore

--- Assets/Client/Scripts/ADSCore.cs
+++ Assets/Client/Scripts/ADSCore.cs
@@ -12,19 +12,11 @@ public class AdsCore : MonoBehaviour, IUnityAdsListener
     
     private string _video = "Interstitial_Android";
     private string _rewardedVideo = "Rewarded_Android";
-    private string _banner = "Banner_Android";
-
+    
     void Start()
     {
         Advertisement.AddListener(this);
         Advertisement.Initialize(_gameId, _testMode);
-        
-        #region Banner
-
-        StartCoroutine(ShowBannerWhenInitialized());
-        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
-        
-        #endregion
     }
 
     public static void ShowAdsVideo(string placementId)
