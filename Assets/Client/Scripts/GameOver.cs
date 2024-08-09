3d78b63b4e241939a715274cd9901260df623ff3
Nastia <adruts@mail.ru>
2024-08-09 23:17:48 +0300

    https://github.com/anastyadru/Tower-Defence.0.git
    Отредактировала скрипт GameOver

--- Assets/Client/Scripts/GameOver.cs
+++ Assets/Client/Scripts/GameOver.cs
@@ -1,5 +1,6 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Advertisements;
